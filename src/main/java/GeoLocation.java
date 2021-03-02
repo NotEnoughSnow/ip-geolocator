1314997e21896cb7d8a2bc4d98a54869e105ad11
Amin Mraidi <amine.mraidi@outlook.com>
2021-03-02 14:00:24 +0100

    added record geolocation to java folder

---
+++ src/main/java/GeoLocation.java
@@ -0,0 +1,6 @@
+package java;
+
+
+record GeoLocation(String as,String city,String country,String countryCode,String isp,double lat,double lon,String org,String query,String region,String regionName,String timezone,String zip) {}
+ 
+
